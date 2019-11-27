01394f74d34bc1d8b8fb405bb36649823457a7ae
Ganna Pavlova <ganna@unumotors.com>
2019-11-27 10:51:36 +0100

    Add week-7 frontend

---
+++ week-7/frontend/src/views/Meetup.vue
@@ -0,0 +1,32 @@
+<script>
+// @ is an alias to /src
+import MeetupCard from '@/components/meetup-card.vue'
+import { mapState, mapActions } from 'vuex'
+
+export default {
+  name: 'Meetup',
+  components: {
+    MeetupCard
+  },
+  computed: {
+    ...mapState(['meetup'])
+  },
+  methods: {
+    ...mapActions(['fetchMeetup'])
+  },
+  created() {
+    this.fetchMeetup(this.$route.params.id)
+  }
+}
+</script>
+
+<template lang="pug">
+  main
+    section
+      meetup-card(:meetup="meetup")
+</template>
+
+<style scoped>
+</style>
+
+
